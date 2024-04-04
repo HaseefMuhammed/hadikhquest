c08a99969a5707b58d9aceffd1d5ecf4fc0d91fb
HaseefMuhammed <haseefmuhammedpc@gmail.com>
2024-04-04 10:14:22 +0530

    ADISOM ADDED

---
+++ adscript.js
@@ -0,0 +1,8 @@
+let ad = document.getElementById("dialog-shower")
+ad.showModal();
+
+function adHold(){
+    let ad = document.getElementById("dialog-shower")
+    ad.close();
+}
+setTimeout(adHold, 11000);
